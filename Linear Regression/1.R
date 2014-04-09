d424c7a51d0487d4f0f1773e276884ec1118a6d9
Hasil Sharma <hasilsharma7@gmail.com>
2014-04-10 00:41:58 +0530

    Basic Linear Regression

--- Linear Regression/1.R
+++ Linear Regression/1.R
@@ -1,12 +1,18 @@
 wine = read.csv("wine.csv")
 model1 = lm(Price ~ AGST,data=wine)
+summary(model1)
 #Multiple R-squared always increase on adding new independent variable
 #but Adjusted R-squared will decrease if a varibable is added that doesn't help
 #the model
 SSE = sum(model1$residuals^2)#Sum of squared errors
+SSE
 model2  = lm(Price ~ AGST+HarvestRain,data=wine)
+summary(model2)
 #Adjusted R squared increases hence harvestrain is good
 SSE = sum(model2$residuals^2)
+SSE
 model3 = lm(Price ~ AGST + HarvestRain + WinterRain + Age + FrancePop,data=wine)
 #using all the variables
+summary(model3)
 SSE = sum(model3$residuals^2)
+SSE
